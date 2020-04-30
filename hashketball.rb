3661a99da197a9cdbe4215a3fc2af93da03c3d8c
Alexandra Radevich <alexa.radevich1@gmail.com>
2020-04-30 14:40:35 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -95,6 +95,17 @@ def game_hash
         :steals => 4,
         :blocks => 5,
         :slam_dunks => 5 
+      },
+      {
+        :player_name => "",
+        :number => ,
+        :shoe => ,
+        :points => ,
+        :rebounds => ,
+        :assists => ,
+        :steals => ,
+        :blocks => ,
+        :slam_dunks => 
       }]
     }
   }
