8a3bb25ccc6ae036de519f5cabc20ecbf841dfd8
Alexandra Radevich <alexa.radevich1@gmail.com>
2020-04-30 14:28:39 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,1 +1,35 @@
 # Write your code here!
+def game_hash
+  gh = {
+    :home => {
+      :team_name => "",
+      :colors => [],
+      :players => [{
+        :player_name => "",
+        :number => "",
+        :shoe => "",
+        :points => "",
+        :rebounds => "",
+        :assists => "",
+        :steals => "",
+        :blocks => "",
+        :slam_dunks => ""
+      }]
+    },
+    :away => {
+      :team_name => "",
+      :colors => [],
+      :players => [{
+        :player_name => "",
+        :number => "",
+        :shoe => "",
+        :points => "",
+        :rebounds => "",
+        :assists => "",
+        :steals => "",
+        :blocks => "",
+        :slam_dunks => ""
+      }]
+    }
+  }
+end 
