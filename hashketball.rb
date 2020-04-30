ab55b0933754b5f8fa3294f249c6019ab0777cf6
Alexandra Radevich <alexa.radevich1@gmail.com>
2020-04-30 14:31:41 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -9,11 +9,11 @@ def game_hash
         :number => 0,
         :shoe => 16,
         :points => 22,
-        :rebounds => "12",
-        :assists => "12",
-        :steals => "3",
-        :blocks => "1",
-        :slam_dunks => "1"
+        :rebounds => 12,
+        :assists => 12,
+        :steals => 3,
+        :blocks => 1,
+        :slam_dunks => 1
       },
       {
         :player_name => "Reggie Evans",
